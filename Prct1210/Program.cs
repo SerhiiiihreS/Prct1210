3abb61f6b404653a8122750977e6ad9869928d4f
Serhii <0992830717s@gmail.com>
2024-10-12 10:18:16 +0200

    I did it.

--- Prct1210/Program.cs
+++ Prct1210/Program.cs
@@ -10,6 +10,52 @@ namespace Prct1210
     {
         static void Main(string[] args)
         {
+            #region Задание  Создайте приложение, которое отображает количество чётных, нечётных, уникальных элементов массива.
+            int[] arr = new int[10];
+            Random rnd = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(-20, 20); // от -20 до +20
+                Console.Write("{0}\t", arr[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("___________________________________");
+            int r = 0;
+            int rd = 0;
+            int sr = 0;
+            int tm = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                tm = arr[i];
+                for (int j = i; j < arr.Length; j++)
+                {
+                    if (arr[j] == tm)
+                    {
+                        sr++;
+                    }
+                }
+            
+                if (sr==1) {
+                    if (arr[i] > 0)
+                    {
+                        rd++;
+                    }
+                    else if (arr[i] < 0)
+                    {
+                        r++;
+                    }
+                }
+                sr = 0;
+                 
+            }
+            Console.WriteLine(rd);
+            Console.WriteLine(r);
+            #endregion
+
+
+            #region
+            #endregion
         }
     }
 }
